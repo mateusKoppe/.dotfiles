098f60fbe8237b33f65b6380f4a90197e0b85556
Mateus Koppe <mateus.koppe@gmail.com>
2018-01-29 00:18:24 -0200

    fixed some commands in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ apt-get update
 apt-get install git
 apt get install vim
 
-mv dotfiles/** ~
+mv ~/dotfiles/* ~
+mv ~/dotfiles/.* ~
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
