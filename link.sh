68a19a0c2001bda7697c388784212ae616f00f9b
Mateus Koppe <mateus.koppe@gmail.com>
2020-02-13 09:50:51 -0300

    Add p10k config

--- link.sh
+++ link.sh
@@ -8,11 +8,13 @@ remove_symlinks(){
   rm -f ${HOME}/.zshrc
   rm -f ${HOME}/.tmux.conf
   rm -f ${HOME}/.config/nvim/init.vim
+  rm -f ${HOME}/.p10k.zsh
 }
 
 create_symlinks(){
   ln -s ${DOTFILES_PATH}/.zshrc ${HOME}/.zshrc
   ln -s ${DOTFILES_PATH}/.tmux.conf ${HOME}/.tmux.conf
+  ln -s ${DOTFILES_PATH}/.p10k.zsh ${HOME}/.p10k.zsh
   mkdir -p ${HOME}/.config/nvim
   ln -s ${DOTFILES_PATH}/init.vim ${HOME}/.config/nvim/init.vim
 }
