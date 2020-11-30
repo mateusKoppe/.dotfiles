f24f41b23bff9444f3062e21ea0dd1381481d7bc
Mateus Koppe <mateus.koppe@gmail.com>
2020-11-29 21:18:21 -0300

    Implement notifications

--- link.sh
+++ link.sh
@@ -5,19 +5,23 @@ PWD=`pwd`
 DOTFILES_PATH="$(dirname $(readlink -f $0))"
 
 remove_symlinks(){
+  rm -fr ${HOME}/.fonts
   rm -f ${HOME}/.zshrc
   rm -f ${HOME}/.tmux.conf
   rm -f ${HOME}/.config/nvim/init.vim
+  rm -f ${HOME}/.config/deadd
   rm -f ${HOME}/.p10k.zsh
-  rm -f ${HOME}/.config/i3 -rf
-  rm -f ${HOME}/.config/polybar -rf
+  rm -f ${HOME}/.config/i3 -r
+  rm -f ${HOME}/.config/polybar -r
+  rm -fr ${HOME}/.config/rofi
 }
 
 create_symlinks(){
   ln -s ${DOTFILES_PATH}/fonts ${HOME}/.fonts
   ln -s ${DOTFILES_PATH}/zsh/.zshrc ${HOME}/.zshrc
+  ln -s ${DOTFILES_PATH}/deadd ${HOME}/.config/deadd
   ln -s ${DOTFILES_PATH}/.tmux.conf ${HOME}/.tmux.conf
-  ln -s ${DOTFILES_PATH}/.p10k.zsh ${HOME}/.p10k.zsh
+  ln -s ${DOTFILES_PATH}/zsh/.p10k.zsh ${HOME}/.p10k.zsh
   mkdir -p ${HOME}/.config/nvim
   ln -s ${DOTFILES_PATH}/nvim/init.vim ${HOME}/.config/nvim/init.vim
   ln -s ${DOTFILES_PATH}/i3 ${HOME}/.config/i3
