2b7e6ce2ef241981cda081ff2b3f2416431ed330
Mateus Koppe <mateus.koppe@gmail.com>
2021-06-22 19:17:06 -0300

    Add screenshot and bluetooth menu

--- bspwm/install.sh
+++ bspwm/install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 sudo pacman -S --noconfirm bspwm sxhkd picom feh polybar xorg-xbacklight rofi xorg-xsetroot
-yay nerd-fonts-fira-code
+yay nerd-fonts-fira-code scrot
 
 DIR="$(dirname $(readlink -f $0))"
 
+rm -r ~/.config/bspwm
 ln -s ${DIR}/bspwm ~/.config/bspwm
+rm -r ~/.config/sxhkd
 ln -s ${DIR}/sxhkd ~/.config/sxhkd
+rm -r ~/.config/polybar
 ln -s ${DIR}/polybar ~/.config/polybar
