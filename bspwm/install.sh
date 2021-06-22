ac5c28681df343882259c5ab9a37ca6340f456e9
Mateus Koppe <mateus.koppe@gmail.com>
2021-06-21 23:25:10 -0300

    Add wallpaper

--- bspwm/install.sh
+++ bspwm/install.sh
@@ -2,9 +2,8 @@
 sudo pacman -S --noconfirm bspwm sxhkd picom feh polybar xorg-xbacklight rofi xorg-xsetroot
 yay nerd-fonts-fira-code
 
-HOME=${HOME}
 DIR="$(dirname $(readlink -f $0))"
 
-ln -s ${DIR}/bspwm ${HOME}/.config/bspwm
-ln -s ${DIR}/sxhkd ${HOME}/.config/sxhkd
-ln -s ${DIR}/polybar ${HOME}/.config/polybar
+ln -s ${DIR}/bspwm ~/.config/bspwm
+ln -s ${DIR}/sxhkd ~/.config/sxhkd
+ln -s ${DIR}/polybar ~/.config/polybar
