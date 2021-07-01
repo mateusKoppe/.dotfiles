c8ba90e38766cd83b061e84c284441c48174f9b7
Mateus Koppe <mateus.koppe@gmail.com>
2021-07-01 16:26:48 -0300

    Colors and rofi

--- bspwm/install.sh
+++ bspwm/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman -S --noconfirm bspwm sxhkd picom polybar xorg-xbacklight rofi xorg-xsetroot brightnessctl nitrogen
+sudo pacman -S --noconfirm bspwm sxhkd picom polybar xorg-xbacklight rofi xorg-xsetroot brightnessctl nitrogen xorg-xrbd
 yay nerd-fonts-fira-code scrot bluez-utils
 
 DIR="$(dirname $(readlink -f $0))"
@@ -10,5 +10,7 @@ rm -r ~/.config/sxhkd
 ln -s ${DIR}/sxhkd ~/.config/sxhkd
 rm -r ~/.config/polybar
 ln -s ${DIR}/polybar ~/.config/polybar
+rm -r ~/.config/rofi
+ln -s ${DIR}/rofi ~/.config/rofi
 
 nitrogen --set-scaled ~/.config/bspwm/wallpaper.jpg
