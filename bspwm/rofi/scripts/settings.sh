f6e6b51d1974af23f5689e16d1e1a9958abc80e8
Mateus Koppe <mateus.koppe@gmail.com>
2021-07-03 02:44:40 -0300

    Implement settings menu

---
+++ bspwm/rofi/scripts/settings.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+selected=$(echo "Network Connect
+Network Settings
+Bluetooth
+Keyboard Layout
+Background
+Cancel" | rofi -dmenu -p "Keyboard layout: ")
+
+if [ "$selected" == "Network Connect" ]; then
+  networkmanager_dmenu
+fi
+if [ "$selected" == "Network Settings" ]; then
+  nm-connection-editor
+fi
+if [ "$selected" == "Bluetooth" ]; then
+  ~/.config/rofi/scripts/bluetooth.sh
+fi
+if [ "$selected" == "Keyboard Layout" ]; then
+  ~/.config/rofi/scripts/keyboard-language.sh
+fi
+if [ "$selected" == "Background" ]; then
+  nitrogen
+fi
