98d4f24d0217a2dd2ad4ecbeb80f2900bac6183c
Mateus Koppe <mateus.koppe@gmail.com>
2022-04-08 15:04:07 -0300

    Add power10k to zsh installating

--- scripts/install-zsh.sh
+++ scripts/install-zsh.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Installing zsh..."
-sudo pacman -Sq stow zsh --noconfirm
+sudo pacman -Sq stow zsh git --noconfirm
 echo "Installing Ohmyzsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 stow zsh
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -128,6 +128,7 @@ export NVM_DIR="/home/koppe/.nvm"
 
 
 alias dev="nvm install && yarn && ./node_modules/.bin/gulp"
+alias awsl="aws --endpoint-url=http://localstack:4566"
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
