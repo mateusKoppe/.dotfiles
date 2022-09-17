210e11f3d86c4969836818aa4c8ca7b1df0be32a
Mateus Koppe <mateus.koppe@gmail.com>
2022-09-17 16:12:02 -0300

    Use JetBrains Mono

--- scripts/setup-neovim.sh
+++ scripts/setup-neovim.sh
@@ -1,5 +1,7 @@
 echo "Installing Neovim..."
 sudo pacman -Sq neovim repgrip --noconfirm
+echo "Installing fonts"
+sudo pacman -S ttf-jetbrains-mono
 echo "Installing Packer..."
 git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 nvim -u ~/.config/nvim/init.lua +PackerInstall
