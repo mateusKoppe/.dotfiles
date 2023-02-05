580ed4e9072bd667242ada8198177ce2de2b818e
Mateus Koppe <mateus.koppe@gmail.com>
2023-02-05 07:02:58 -0300

    Set lock for i3

--- i3/scripts/lock.sh
+++ i3/scripts/lock.sh
@@ -2,4 +2,4 @@
 #
 WALLPAPER=`cat .config/nitrogen/bg-saved.cfg | grep -m 1 file | cut -d "=" -f 2`
 
-convert -resize 1920x1080 $WALLPAPER RGB:- | i3lock -t -i /dev/stdin --raw=1920x1080:rgb
+convert -resize 1920x1080 $WALLPAPER RGB:- | i3lock -t -i /dev/stdin --raw=1920x1080:rgb --no-fork
