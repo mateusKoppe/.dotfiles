c1cfbe819d213765171481a18196d4dbda252038
Mateus Koppe <mateus.koppe@gmail.com>
2024-02-21 14:48:09 -0300

    Fix brightctl permission

--- awesome/README.md
+++ awesome/README.md
@@ -1,5 +1,9 @@
 # AwesomeWm Config
 
+## TODO:
+- [] Select audio and mic
+- [] Fix volume changes for bluetooth devices
+
 ## Dependencies
 - Awesome
 - Picom
--- awesome/setup.sh
+++ awesome/setup.sh
@@ -4,12 +4,17 @@
 echo "Installing AwesomeWM and dependencies..."
 
 if [[ ! -z $(which apt-get) ]]; then
-  sudo apt-get install -y awesome picom xclip scrot ranger
+  sudo apt-get install -y awesome picom xclip scrot ranger i3lock brightnessctl
 elif [[ ! -z $(which pacman) ]]; then
-  sudo pacman -S --noconfirm awesome picom xclip scrot ranger
+  sudo pacman -S --noconfirm awesome picom xclip scrot ranger i3lock brightnessctl
 else
   echo "Package manager not found"
 fi
+
+echo "Add user to video group"
+echo "  (to control brightness)"
+
+sudo usermod -aG video $USER
 
 stow awesome
 
