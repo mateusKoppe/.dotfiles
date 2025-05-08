522ed6870ab0b20058db5270d7f5c66bbe6ab050
Mateus Koppe <mateus.koppe@gmail.com>
2025-05-08 16:16:27 -0300

    Fix awesomewm font

--- awesome/.config/awesome/core/theme.lua
+++ awesome/.config/awesome/core/theme.lua
@@ -11,7 +11,7 @@ local M = {}
 M.theme = {}
 local palette = require("core.colors").mocha
 
-M.theme.font = "JetBrains Mono Regular 9"
+M.theme.font = "JetBrainsMono NF Regular 9"
 
 M.theme.bg_normal   = palette.base
 M.theme.bg_focus    = palette.crust
@@ -29,19 +29,6 @@ M.theme.border_width  = dpi(1)
 M.theme.border_normal = palette.crust
 M.theme.border_focus  = palette.blue
 M.theme.border_marked = palette.maroon
-
--- There are other variable sets
--- overriding the default one when
--- defined, the sets are:
--- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
--- tasklist_[bg|fg]_[focus|urgent]
--- titlebar_[bg|fg]_[normal|focus]
--- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
--- mouse_finder_[color|timeout|animate_timeout|radius|factor]
--- prompt_[fg|bg|fg_cursor|bg_cursor|font]
--- hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
--- Example:
---theme.taglist_bg_focus = "#ff0000"
 
 -- Generate taglist squares:
 local taglist_square_size = dpi(4)
