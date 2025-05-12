665f91e25bcd6a4acd6b3bff101f49aaf8c8783f
Mateus Koppe <mateus.koppe@gmail.com>
2025-05-11 23:51:59 -0300

    Fix nvim ln

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -9,7 +9,7 @@ if $UBUNTU; then
   ## Ubuntu provies a old version
   if ! [ -x "$(command -v nvim)" ]; then
     curl -s -L https://github.com/neovim/neovim/releases/download/v0.11.1/nvim-linux-x86_64.tar.gz | sudo tar xvz -C /opt
-    ln -s /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
+    sudo ln -s /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
   fi
 fi
 
